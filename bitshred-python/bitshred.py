f2891fa21739824673be23e28d55e23a6632c682
Jerry Yang <jerry871002@gmail.com>
2023-10-27 12:09:57 +0200

    shred the executable section into n-grams

--- bitshred-python/bitshred.py
+++ bitshred-python/bitshred.py
@@ -11,12 +11,13 @@ import pefile
 @total_ordering
 @dataclass
 class Address:
+    """A class to print the address in hex format in repr"""
     address: int
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return hex(self.address)
     
-    def __add__(self, other):
+    def __add__(self, other: 'Address | int') -> 'Address':
         try:
             return Address(self.address + other.address)
         except AttributeError:
@@ -28,7 +29,7 @@ class Address:
             logging.debug(f'Other is also not an integer')
             raise
     
-    def __lt__(self, other):
+    def __lt__(self, other: 'Address') -> bool:
         return self.address < other.address
 
 @dataclass
@@ -50,9 +51,23 @@ def main(args: argparse.Namespace):
     for root, _, files in os.walk(args.binary):
         for file in files:
             sample = os.path.join(root, file)
+
             binary_file = initailaize_binary_file(sample)
-            if binary_file:
-                shred_section(binary_file, args.shred_size)
+            if not binary_file:
+                continue
+            
+            logging.debug(binary_file)
+            shred_hashes = shred_section(binary_file, args.shred_size)
+
+            if len(shred_hashes) < args.window_size:
+                logging.warning(f'{sample} skipped (no appropriate sections)')
+                continue
+
+            # TODO: create fingerprint
+            for i in range(len(shred_hashes) - args.window_size + 1):
+                pass
+
+
 
 def initailaize_binary_file(file_path: str) -> BinaryFile:
     try:
@@ -73,6 +88,7 @@ def initailaize_binary_file(file_path: str) -> BinaryFile:
         Section(
             name=section.Name.decode().rstrip('\x00'),
             data=section.get_data(),
+            # TODO: should we use SizeOfRawData or Misc_VirtualSize?
             data_size=section.SizeOfRawData,
             vma=Address(pe.OPTIONAL_HEADER.ImageBase+section.VirtualAddress),
             is_code=section.IMAGE_SCN_CNT_CODE
@@ -84,8 +100,10 @@ def initailaize_binary_file(file_path: str) -> BinaryFile:
         sections=sections_data
     )
 
-def shred_section(binary_file: BinaryFile, shred_size: int):
+def shred_section(binary_file: BinaryFile, shred_size: int) -> list[int]:
     logging.debug(f'Shredding {binary_file.filename}')
+
+    shred_hashes = []
     for section in binary_file.sections:
         if (
             not section.is_code or
@@ -102,13 +120,30 @@ def shred_section(binary_file: BinaryFile, shred_size: int):
         logging.debug(f'Processing section {section.name}: {section.data_size=}, {shred_size=}')
 
         section_shred_num = section.data_size - shred_size + 1
+        for i in range(section_shred_num):
+            shred = section.data[i:i+shred_size]
+            shred_hash = djb2_hash(shred)
+            shred_hashes.append(shred_hash)
+
+        logging.debug(f'Finished processing section {section.name}')
+        logging.debug(f'{shred_hashes[:5]=}')
+
+    return shred_hashes
 
 
+def djb2_hash(data: bytes) -> int:
+    hash = 5381
+    for byte in data:
+        hash = hash * 33 + byte
+    # limits the hash to 32 bits (unsigned int)
+    return hash & 0xFFFFFFFF
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='BitShred reimplementation in Python')
     parser.add_argument('-b', '--binary', help='Path to directory that contains binary samples')
     parser.add_argument('-s', '--shred-size', help='Shred size', default=4, type=int)
+    parser.add_argument('-w', '--window-size', help='Window size', default=1, type=int)
+    parser.add_argument('--fp-size', help='Fingerprint size (in KB)', default=32, type=int)
     args = parser.parse_args()
     main(args)
